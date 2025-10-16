01e49391319244b232a871b79d70bad65b713bfd
mvntahax <syedamuntahapk@gmail.com>
2025-10-16 16:14:39 +0500

    completed 4th task.

---
+++ task-4/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// function App() is a React Component
+const App = () => {
+  return ( // returns a JSX syntax
+    <div>
+      <h1>Hello React!</h1>
+      <p>Welcome to my first react component!</p>
+    </div>
+  )
+}
+
+export default App // allows other files to use it
